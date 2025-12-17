324adf05d4fe216bd5cb6d640db3f736d8b29bc7
Rene Kathofer <mail@rene-kathofer.at>
2025-12-17 19:25:49 +0100

    feat: Initialize Gradle project with Spring Boot setup

---
+++ src/main/java/at/rkathofer/camunda_challenge/CamundaChallengeApplication.java
@@ -0,0 +1,13 @@
+package at.rkathofer.camunda_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamundaChallengeApplication.class, args);
+    }
+
+}
